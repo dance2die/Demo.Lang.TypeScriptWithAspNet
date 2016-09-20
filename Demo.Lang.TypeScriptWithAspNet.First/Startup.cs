88ffdb14896375117b5072b7482db26cbf9a2f19
dance2die <dontbothermewithspam@gmail.com>
2016-09-20 12:43:48 -0400

    Initial Import: Typescript configured.

---
+++ Demo.Lang.TypeScriptWithAspNet.First/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Demo.Lang.TypeScriptWithAspNet.First.Startup))]
+namespace Demo.Lang.TypeScriptWithAspNet.First
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
